65fbd52fd6395b12cde9ac6ab7069a21a1679197
AEZOS <thexxxxx15@gmail.com>
2026-02-27 19:14:57 +0200

    fix: increased gunicorn worker timeout from default 30s to 120s to prevent early term of gemini generations

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Start the Flask Server in the background
-gunicorn shapeshift_server:app --bind 0.0.0.0:$PORT &
+gunicorn shapeshift_server:app --bind 0.0.0.0:$PORT --timeout 120 &
 
 # Wait a second for gunicorn
 sleep 2
